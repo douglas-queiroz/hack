3aceabe9d6202ad2a3bdc8add773640ad8f51524
Douglas Queiroz <douglasq16@gmail.com>
2016-06-20 22:45:41 -0300

    Funcoes play e stop

--- Hack/ViewController.swift
+++ Hack/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer!
 
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var btnStop: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func playAudio()
+    {
+        let soundURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("4estacoes", ofType: "mp3")!)
+        do
+        {
+            audioPlayer = try AVAudioPlayer(contentsOfURL: soundURL)
+            audioPlayer!.delegate = self
+            if (audioPlayer!.prepareToPlay())
+            {
+                audioPlayer!.play()
+            }
+        }
+        catch
+        { }
     }
-
-
+    
+    @IBAction func btnPlayAction(sender: AnyObject) {
+        UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
+        let commandCenter = MPRemoteCommandCenter.sharedCommandCenter()
+        commandCenter.playCommand.enabled = true
+        commandCenter.stopCommand.enabled = true
+        commandCenter.playCommand.addTarget(self, action: #selector(play))
+        commandCenter.stopCommand.addTarget(self, action: #selector(stop))
+        
+        self.play()
+    }
+    
+    @IBAction func btnStopAction(sender: AnyObject) {
+        self.stop()
+    }
+    
+    func play() {
+        self.playAudio()
+        btnPlay.enabled = false
+        btnStop.enabled = true
+    }
+    
+    func stop() {
+        audioPlayer!.stop()
+        btnPlay.enabled = true
+        btnStop.enabled = false
+    }
 }
 
